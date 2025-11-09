33816ee92fd6a187a81d656ef290211197ef91f6
Munewxar <steven.altamirano.work@gmail.com>
2025-11-09 22:09:24 +0300

    update js

--- staticfiles/js/api.js
+++ staticfiles/js/api.js
@@ -1,5 +1,5 @@
- export const SITE = "http://localhost:8000";
-//export const SITE = "https://nidjatstore.ru";
+// export const SITE = "http://localhost:8000";
+export const SITE = "https://nidjatstore.ru";
 
 export const sendFetchPost = (page, body, funcs) => {
     fetch(`${SITE}/api/${page}`, {
